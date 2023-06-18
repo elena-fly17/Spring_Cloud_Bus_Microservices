1a5415cc77446c079217ddebd9be672f85eb6a90
elena-fly17 <elena17mr@gmail.com>
2023-06-18 17:01:15 +0300

    Заменила application.properties в сервисах eureka-server и config-server на application.yml

--- client_1/src/main/java/com/example/client_1/Client1Application.java
+++ client_1/src/main/java/com/example/client_1/Client1Application.java
@@ -2,6 +2,7 @@ package com.example.client_1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 public class Client1Application {
